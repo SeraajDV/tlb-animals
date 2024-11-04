ea4df558ceeadfd238fd5c46fd4c60c156c0437e
SeraajDV <seraajdevilliers@gmail.com>
2024-11-04 21:04:21 +0200

    create custom hook to make use of localstorage which will be used in the animal details page and used for favourites page

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+export const useLocalStorage = <T>(key: string, initialValue: T): [T, (value: T) => void] => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(`Error reading from local storage: ${error}`);
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(storedValue));
+    } catch (error) {
+      console.error(`Error writing to local storage: ${error}`);
+    }
+  }, [key, storedValue]);
+
+  return [storedValue, setStoredValue];
+};
