405e37bfbddd80af178081df0757ac8f4c7ed091
SeraajDV <seraajdevilliers@gmail.com>
2024-11-04 20:36:30 +0200

    use TanStack query to fetch and cache data and to see the shape of the data on the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAnimals } from "../api/animals";
+
 export default function Home() {
-  return <div>Home</div>;
+  const { data } = useQuery({
+    queryKey: ["animals"],
+    queryFn: () => getAnimals("dog"),
+  })
+  return(
+    <div>
+      <h1>Home</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
